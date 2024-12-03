f302d4b0016a01decd966632ed838b10e38db075
Mubeolli <mubhoney@yahoo.com>
2024-12-03 09:40:22 -0800

    category editted

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { categoryImage } from './CategoryFullinfos'
+import { categoryImage } from './CategoryFullInfos'
 import CategoryCard from './CategoryCard'
 import classes from './Category.module.css'
 
